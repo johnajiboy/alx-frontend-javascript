5838bdc6ae0d14950b71df6bf852c8d9940811df
johnajiboy <johnajiboye53@gmail.com>
2023-08-01 21:07:11 +0000

    classes

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,11 @@
+
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+}
+
+Airport.prototype.toString = function airportToString() {
+  return `[object ${this._code}]`;
+};
